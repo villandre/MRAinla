9578289896734b7c3de9dc9d04c1b0ec15094fc9
villandre <luc.villandre@mail.mcgill.ca>
2019-11-01 19:40:35 -0400

    Still fixing the software. Will need to go back to lon-lat, rather than lat-lon...

--- R/miscFunctions.R
+++ R/miscFunctions.R
@@ -1,8 +1,8 @@
 # spaceDistFun must take as an argument a matrix or data.frame with a column named latitude and another called longitude.
 
-SimulateSpacetimeData <- function(numObsPerTimeSlice = 225, covFunction, latRange, lonRange,  timeValuesInPOSIXct, covariateGenerationFctList, errorSD, spaceDistFun, timeDistFun, FEvalues) {
+SimulateSpacetimeData <- function(numObsPerTimeSlice = 225, covFunction, lonRange, latRange,  timeValuesInPOSIXct, covariateGenerationFctList, errorSD, spaceDistFun, timeDistFun, FEvalues) {
   numSlotsPerRow <- ceiling(sqrt(numObsPerTimeSlice))
-  slotCoordinates <- sapply(list(latitude = latRange, longitude = lonRange), FUN = function(x) {
+  slotCoordinates <- sapply(list(longitude = lonRange, latitude = latRange), FUN = function(x) {
     width <- abs(diff(x)/numSlotsPerRow)
     seq(from = min(x) + width/2, to = max(x) - width/2, length.out = numSlotsPerRow)
   }, USE.NAMES = TRUE)
@@ -19,21 +19,22 @@ SimulateSpacetimeData <- function(numObsPerTimeSlice = 225, covFunction, latRang
   coordinates$time <- rep(timeValuesInPOSIXct, each = numObsPerTimeSlice)
 
   covariateMatrix <- cbind(1, do.call("cbind", lapply(covariateGenerationFctList, function(x) {
-    covariateValues <- x(coordinates[, c("latitude", "longitude")], coordinates[, "time"])
+    covariateValues <- x(coordinates[, c("longitude", "latitude")], coordinates[, "time"])
     if (is.null(dim(covariateValues))) {
       dim(covariateValues) <- c(length(covariateValues), 1)
     }
     covariateValues
   })))
 
-  spatialDistMatrix <- spaceDistFun(coordinates[, c("latitude", "longitude")])
+  spatialDistMatrix <- spaceDistFun(coordinates[ , c("longitude", "latitude")])
   timeDistMatrix <- timeDistFun(coordinates[, "time"])
   covarianceMat <- covFunction(spatialDistMatrix, timeDistMatrix)
   meanVector <- drop(covariateMatrix %*% FEvalues)
   fieldValues <- drop(mvtnorm::rmvnorm(n = 1, mean = meanVector, sigma = covarianceMat)) + rnorm(n = length(meanVector), mean = 0, sd = errorSD)
   dataForObject <- cbind(y = fieldValues, as.data.frame(covariateMatrix[, -1]))
   colnames(dataForObject) <- c("y", paste("Covariate", 1:(length(FEvalues) - 1), sep = ""))
-  spacetimeObj <- spacetime::STIDF(sp = sp::SpatialPoints(coordinates[, c("latitude", "longitude")]), data = dataForObject, time = coordinates$time)
+  spacetimeObj <- spacetime::STIDF(sp = sp::SpatialPoints(coordinates[, c("longitude", "latitude")]), data = dataForObject, time = coordinates$time)
+  colnames(spacetimeObj@sp@coords) <- c("longitude", "latitude")
   spacetimeObj
 }
 
@@ -122,4 +123,28 @@ plotOutput <- function(inlaMRAoutput, trainingData, testData, realTestValues = N
     dev.off()
   }
   invisible(0)
+}
+
+plotSpacetimeData <- function(spacetimeData) {
+
+  width <- ceiling(sqrt(nrow(spacetimeData@sp@coords)/length(unique(time(spacetimeData@time)))))
+  padding <- 0.1
+  landRaster <- raster::raster(nrows = width, ncols = width, xmn = min(spacetimeData@sp@coords[ , 1]) - padding, xmx = max(spacetimeData@sp@coords[ , 1]) + padding, ymn = min(spacetimeData@sp@coords[ , 2]) - padding, ymx = max(spacetimeData@sp@coords[ , 2]) + padding)
+  getTestRaster <- function(dayIndex) {
+
+    desiredTime <- sort(unique(time(spacetimeData@time)))[[dayIndex]]
+
+    timeIndicesTest <- which(time(spacetimeData@time) == desiredTime)
+
+    subSpacetimeData <- spacetimeData[timeIndicesTest]
+
+    testPoints <- sp::SpatialPoints(subSpacetimeData@sp@coords)
+
+    raster::rasterize(x = testPoints, y = landRaster, field = subSpacetimeData@data$y)
+  }
+  rasterList <- lapply(seq_along(unique(time(spacetimeData@time))), getTestRaster)
+  stackedRasters <- stack(rasterList)
+  raster::spplot(stackedRasters, scales = list(draw = TRUE),
+         xlab = "Longitude", ylab = "Latitude",
+         names.attr = as.character(unique(time(spacetimeData))))
 }
