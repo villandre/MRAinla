6198eebec71866d961c7db8d60f249399221440f
villandre <luc.villandre@mail.mcgill.ca>
2020-03-19 15:54:50 -0400

    Still fixing plot functions.

--- R/plotFunctions.R
+++ R/plotFunctions.R
@@ -31,9 +31,9 @@ plot.INLAMRA <- function(x, filename = NULL, type = c("joint", "training", "pred
   }
 
   if (control$plotRaster & (type != "marginals")) {
-    plottedObjects <- .plotRaster(x, control, type = type, ...)
+    plottedObjects <- .plotRaster(x, control, filename = filename, type = type, ...)
   } else if (type[[1]] != "marginals") {
-    plottedObjects <- .plotPoints(x, control, type = type, ...)
+    plottedObjects <- .plotPoints(x, control, filename = filename, type = type, ...)
   } else {
     plottedObjects <- .plotMarginals(output = x, filename = filename, device = control$graphicsEngine, ...)
   }
